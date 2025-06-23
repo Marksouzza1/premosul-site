22d6f48ece2c3ef2cb352313d7aea6d5f97b8664
Marksouzza1 <marcogs713@gmail.com>
2025-06-23 18:05:16 -0300

    added service component

---
+++ app/components/Serviços/Servicos.jsx
@@ -0,0 +1,57 @@
+'use client';
+
+import styles from "@/app/components/Serviços/Servico.module.scss";
+import Image from "next/image";
+
+export default function Servicos() {
+    const cards = [
+        {
+            src: "/images/servico1.jpg",
+            alt: "Construção de galpão pré-moldado",
+            title: "Galpões Industriais",
+            description: "Eficiência e resistência para projetos de grande porte."
+        },
+        {
+            src: "/images/servico2.jpg",
+            alt: "Casas geminadas modernas",
+            title: "Casas Geminadas",
+            description: "Conforto, economia e aproveitamento inteligente do espaço urbano."
+        },
+        {
+            src: "/images/servico3.jpg",
+            alt: "Soluções residenciais completas",
+            title: "Projetos Personalizados",
+            description: "Adaptamos nossos pré-moldados às suas necessidades."
+        }
+    ];
+
+    return (
+        <section className={styles.services}>
+            <div className={styles.header}>
+                <h2 className={styles.title}>
+                    Soluções Premol Sul para Você
+                </h2>
+                <p className={styles.subtitle}>
+                    Na Premol Sul, oferecemos soluções completas e eficientes para cada tipo de construção, garantindo que seu projeto seja entregue com qualidade e no prazo certo.
+                </p>
+            </div>
+            <div className={styles.grid}>
+                <div className={styles.cardImage}>
+                    {cards.map((card, index) => (
+                        <div key={index} className={styles.card}>
+                            <Image
+                                src={card.src}
+                                alt={card.alt}
+                                width={403}
+                                height={250}
+                                className={styles.image}
+                            />
+                            <h3 className={styles.cardTitle}>{card.title}</h3>
+                            <p className={styles.cardDescription}>{card.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/app/components/Hero/index.jsx"
+import Servicos from "./components/Serviços/Servicos"
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Servicos />
     </div>
   )
 }
